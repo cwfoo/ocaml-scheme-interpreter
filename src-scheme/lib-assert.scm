41071515447ea5add3df220592d72ac040b890bc
Foo Chuan Wei <chuanwei.foo@hotmail.com>
2021-01-19 14:08:01 +0800

    Print the test cases when they are running

--- src-scheme/lib-assert.scm
+++ src-scheme/lib-assert.scm
@@ -1,16 +1,31 @@
 ;;;; Library useful for unit tests.
 
-(define (assert-true v)
-  (if v
-    (displayln "OK")
-    (error "ASSERT-TRUE failed.")))
+(define-macro assert-true
+  (lambda (exp)
+    (list 'cond (list (cadr exp)
+                      (list 'display "OK: ")
+                      (list 'displayln (list 'quote exp)))
+                (list 'else
+                      (list 'display "FAILED: ")
+                      (list 'displayln (list 'quote exp))
+                      (list 'error "ASSERT-TRUE failed.")))))
 
-(define (assert-false v)
-  (if v
-    (error "ASSERT-FALSE failed.")
-    (displayln "OK")))
+(define-macro assert-false
+  (lambda (exp)
+    (list 'cond (list (list 'not (cadr exp))
+                      (list 'display "OK: ")
+                      (list 'displayln (list 'quote exp)))
+                (list 'else
+                      (list 'display "FAILED: ")
+                      (list 'displayln (list 'quote exp))
+                      (list 'error "ASSERT-FALSE failed.")))))
 
-(define (assert-equal a b)
-  (if (equal? a b)
-    (displayln "OK")
-    (error "ASSERT-EQUAL failed.")))
+(define-macro assert-equal
+  (lambda (exp)
+    (list 'cond (list (list 'equal? (cadr exp) (caddr exp))
+                      (list 'display "OK: ")
+                      (list 'displayln (list 'quote exp)))
+                (list 'else
+                      (list 'display "FAILED: ")
+                      (list 'displayln (list 'quote exp))
+                      (list 'error "ASSERT-EQUAL failed.")))))
